091bf4d3b465323d8cfd8fc870a6592db3d008ea
DaiZhiTao <m13007230048@163.com>
2017-09-06 23:52:29 +0800

    使用semantic

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
+app.use('/admin', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,10 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// 默认打开广场
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user/index', { title: '用户主页' });
+  res.send('后台页面');
 });
-
-
-// 朋友圈
-  router.get('/friends-circle',function(req, res, next) {
-    res.render('user/friends-circle', { title: '朋友圈'});
-  });
-
-// 我的收藏
-  router.get('/my-collections',function(req, res, next) {
-    res.render('user/my-collections', { title: '我的收藏'});
-  });
-
-// 我的攻略
-  router.get('/my-raiders',function(req, res, next) {
-    res.render('user/my-raiders', { title: '我的攻略'});
-  });
 
 module.exports = router;
